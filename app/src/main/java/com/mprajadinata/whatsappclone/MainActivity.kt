40661dda893bf7682a1b5e76d70fc0d6d4808690
Praja510 <moh.prajadinata@gmail.com>
2020-10-20 10:02:34 +0700

    newest

--- app/src/main/java/com/mprajadinata/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/mprajadinata/whatsappclone/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
@@ -36,6 +37,33 @@ class MainActivity : AppCompatActivity() {
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
         resizeTabs()
         tabs.getTabAt(1)?.select()
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                when (tab?.position) {
+                    0 -> fab.hide()
+                    1 -> fab.show()
+                    2 -> fab.hide()
+                }
+            }
+        })
+
+        fab.setOnClickListener {
+            onNewChat()
+            Snackbar.make(it, "Replace with action", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show()
+
+        }
+    }
+
+    private fun onNewChat() {
+        Toast.makeText(this, "Add action for result", Toast.LENGTH_LONG).show()
     }
 
     private fun resizeTabs() {
--- app/src/main/java/com/mprajadinata/whatsappclone/adapter/SectionPagerAdapter.kt
+++ app/src/main/java/com/mprajadinata/whatsappclone/adapter/SectionPagerAdapter.kt
@@ -4,17 +4,28 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.mprajadinata.whatsappclone.MainActivity
+import com.mprajadinata.whatsappclone.fragment.ChatsFragment
+import com.mprajadinata.whatsappclone.fragment.StatusListFragment
+import com.mprajadinata.whatsappclone.fragment.StatusUpdateFragment
 
 class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getItem(position: Int): Fragment {
 
-        return MainActivity.PlaceHolderFragment.newIntent(
-            position + 1
-        )
+    private val chatsFragment = ChatsFragment()
+    private val statusUpdateFragment = StatusUpdateFragment()
+    private val statusFragment = StatusListFragment()
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> statusUpdateFragment
+            1 -> chatsFragment
+            2 -> statusFragment
+
+            else -> chatsFragment
+        }
     }
 
     override fun getCount(): Int {
-
         return 3
+
     }
 }

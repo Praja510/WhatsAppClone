f337729a0faf0d9941aeae13d7eaca6314d44362
Praja510 <moh.prajadinata@gmail.com>
2020-10-23 09:21:44 +0700

    newest

--- app/src/main/java/com/mprajadinata/whatsappclone/adapter/ChatsAdapter.kt
+++ app/src/main/java/com/mprajadinata/whatsappclone/adapter/ChatsAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mprajadinata.whatsappclone.R
 import com.mprajadinata.whatsappclone.fragment.ChatsFragment
 import com.mprajadinata.whatsappclone.listener.ChatClickListener
-import com.mprajadinata.whatsappclone.util.populateImage
+import com.mprajadinata.whatsappclone.util.*
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_chats.*
 
@@ -44,10 +46,62 @@ class ChatsAdapter(val chats: ArrayList<String>) :
     class ChatsViewHolder(override val containerView: View) :
         RecyclerView.ViewHolder(containerView), LayoutContainer {
 
+        private val firebaseDb = FirebaseFirestore.getInstance()
+        private val userId = FirebaseAuth.getInstance().currentUser?.uid
+        private var partnerId: String? = null
+        private var chatName: String? = null
+        private var chatImageUrl: String? = null
         fun bindItem(chatId: String, listener: ChatClickListener?) {
-            populateImage(img_chats.context, "", img_chats, R.drawable.ic_user)
-            txt_chats.text = chatId
+            progress_layout.visibility = View.VISIBLE
+            progress_layout.setOnTouchListener { v, event -> true }
 
+            firebaseDb.collection(DATA_CHATS)
+                .document(chatId)
+                .get()
+                .addOnSuccessListener {
+                    val chatParticipants = it[DATA_CHAT_PARTICIPANTS]
+                    if (chatParticipants != null) {
+                        for (participant in chatParticipants as ArrayList<String>) {
+                            if (participant != null && !participant.equals(userId)) {
+                                partnerId = participant
+                                firebaseDb.collection(DATA_USERS).document(partnerId!!).get()
+                                    .addOnSuccessListener {
+                                        val user = it.toObject(User::class.java)
+                                        chatImageUrl = user?.imageUrl
+                                        chatName = user?.name
+                                        txt_chats.text = user?.name
+                                        populateImage(
+                                            img_chats.context,
+                                            user?.imageUrl,
+                                            img_chats,
+                                            R.drawable.ic_user
+                                        )
+                                        progress_layout.visibility = View.GONE
+
+                                    }
+
+                                    .addOnFailureListener { e ->
+                                        e.printStackTrace()
+                                        progress_layout.visibility = View.GONE
+
+                                    }
+                            }
+                        }
+                    }
+
+                    progress_layout.visibility = View.GONE
+
+                }
+
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                    progress_layout.visibility = View.GONE
+
+                }
+
+            itemView.setOnClickListener {
+                listener?.onChatClicked(chatId, userId, chatImageUrl, chatName)
+            }
         }
     }
 }
